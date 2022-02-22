fc1bfa6a811d52f47dc11c46b824119884caef40
Orumaito <77343934+AllMightHero@users.noreply.github.com>
2022-02-22 11:22:34 -0600

    Add files via upload

--- plugins/ecchi.js
+++ plugins/ecchi.js
@@ -5,7 +5,7 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Sexy ❤️‍🔥*", '©𝓝𝓮𝔃𝓾𝓴𝓸𝓑𝓸𝓽, '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Sexy ❤️‍🔥*", '©𝓝𝓮𝔃𝓾𝓴𝓸𝓑𝓸𝓽', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^ecchi$/i
 module.exports = handler
 module.exports = handler
