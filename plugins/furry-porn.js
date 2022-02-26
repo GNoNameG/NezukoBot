3b943421e93d29abc5ef5d1ce941ef155ea6c099
Orumaito <77343934+AllMightHero@users.noreply.github.com>
2022-02-25 21:06:41 -0600

    Update furry-porn.js

--- plugins/furry-porn.js
+++ plugins/furry-porn.js
@@ -5,6 +5,6 @@ let json = res.data
 let url = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.send2ButtonImg(m.chat, url, "*¿Si me pongo de perrito, me adoptas? 🔥🥴*", '©', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }})}
+conn.send2ButtonImg(m.chat, url, "*¿Si me pongo de perrito, me adoptas? 🔥🥴*", '©𝕆𝕣𝕦𝕞𝕒𝕚𝕥𝕠-𝓝𝓮𝔃𝓾𝓴𝓸𝓑𝓸𝓽', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(furry|furro|furr@|furra|furrys|furros|furr@s|furras|furri|furris)$/i
 module.exports = handler
